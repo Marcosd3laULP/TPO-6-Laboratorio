37c52cc520fcf93ecb0ca3943a391c7470befbd4
AnddyPest <mauripestchanker@gmail.com>
2024-09-09 16:31:41 -0300

    Creados elementos de clase

--- JavaApplication54/src/clases/principal/Productos.java
+++ JavaApplication54/src/clases/principal/Productos.java
@@ -1,11 +1,96 @@
-
-
 package clases.principal;
 
-/**
- *
- * @author mauri
- */
-public class Productos {
+public class Productos implements Comparable<Integer>{
+    
+    int code;
+    String name;
+    String brand;
+    String type;
+    double price;
+    int stock;
+
+    public Productos(int code, String name, String brand, String type, double price, int stock) {
+        this.code = code;
+        this.name = name;
+        this.brand = brand;
+        this.type = type;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 89 * hash + this.code;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Productos other = (Productos) obj;
+        return this.code == other.code;
+    }
+
+    @Override
+    public int compareTo(Integer o) {
+        return o.compareTo(code);
+    }
 
 }
--- JavaApplication54/src/entidades/clases/EntidadesProductos.java
+++ JavaApplication54/src/entidades/clases/EntidadesProductos.java
@@ -1,11 +1,29 @@
-
-
 package entidades.clases;
 
-/**
- *
- * @author mauri
- */
+import clases.principal.Productos;
+import java.util.TreeSet;
+
 public class EntidadesProductos {
 
+    TreeSet<Productos> products;
+
+    public EntidadesProductos() {
+        this.products = new TreeSet();
+    }
+
+    public EntidadesProductos(TreeSet<Productos> products) {
+        this.products = products;
+    }
+
+    public TreeSet<Productos> getProducts() {
+        return products;
+    }
+
+    public void setProducts(TreeSet<Productos> products) {
+        this.products = products;
+    }
+    
+    
+    
+    
 }
